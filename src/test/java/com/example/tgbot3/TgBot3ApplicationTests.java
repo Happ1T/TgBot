2e35170a0cdcb241e30dc577a845a9bd28add0c8
Happ1T <happyplayy@mail.ru>
2022-10-06 20:41:15 +0300

    1st fixed vers

---
+++ src/test/java/com/example/tgbot3/TgBot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tgbot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
